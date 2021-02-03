5b5f9aa6a884acc6e07e9405de57b97184e3b457
Anthony Howell <anthonyamh@gmail.com>
2021-02-02 21:46:13 -0600

    Finished basic application functionality

--- app/src/main/java/com/example/lab2b/MainActivity.java
+++ app/src/main/java/com/example/lab2b/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lab2b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onClickInputButtonHelp(View v) {
+        String[] sentences = getResources().getStringArray(R.array.sentences);
+        int idx = new Random().nextInt(sentences.length);
+        String s = (sentences[idx]);
 
+        TextView t = (TextView) findViewById(R.id.textOutput);
+        t.setText(s);
+    }
 }
